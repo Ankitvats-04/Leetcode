8166c2b2119373fe1bff8fd7f7be87f294a82e31
Ankitvats-04 <panditankit1456@gmail.com>
2026-09-09 18:27:36 +0530

    Time: 24 ms (52.99%), Space: 65.5 MB (41.29%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0525-contiguous-array/0525-contiguous-array.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int findMaxLength(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(0,-1);
+        int max=0;
+        int sum =0;
+        for(int i=0; i<nums.length;i++){
+            if(nums[i]==0){
+                sum+=-1;
+            }
+            else{
+                sum+=1;
+            }
+            if(map.containsKey(sum)){
+                int length = i-map.get(sum);
+                max = Math.max(max,length);
+            }
+            else{
+                map.put(sum,i);
+            }
+        }
+        return max;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
