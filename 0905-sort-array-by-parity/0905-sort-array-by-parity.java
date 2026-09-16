f691cfa6f04206fcca5eb4377ff0340a9cd4a6d4
Ankitvats-04 <panditankit1456@gmail.com>
2026-09-17 00:37:48 +0530

    Time: 0 ms (100%), Space: 46.7 MB (69.41%) - LeetHub - Time: O(n), Space: O(1)

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.java
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.java
@@ -1,5 +1,7 @@
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
+
+        // Using two pointer in the same direction
         int left =0;
         for(int right = 0; right<nums.length; right++){
             if(nums[right]%2==0){
@@ -12,6 +14,8 @@ class Solution {
         return nums;
     }
 }
+
+// solve using two pointer opposite direction
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
